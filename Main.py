9c777b44a78c573b735f60df25d545e112ce71c4
dawnmv <109648022+dawnmv@users.noreply.github.com>
2022-07-26 21:38:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ class Stack:
 
     def is_empty(self):
         # Write code here
-        if len(self.items)==0:
+        if len(self.items) == 0:
             return True
         else:
             return False
@@ -14,7 +14,7 @@ class Stack:
 
     def is_full(self):
         # Write code here
-        if len(self.items)==self.size:
+        if len(self.items) == self.size:
             return True
         else:
             return False
